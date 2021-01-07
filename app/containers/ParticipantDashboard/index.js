f2c7654f97d91ac52c873cdeefa8154a3649c944
Chaya Kleiner <ckleiner@infinidat.com>
2021-01-07 21:37:19 +0200

    Add Sort Functions

--- app/containers/ParticipantDashboard/index.js
+++ app/containers/ParticipantDashboard/index.js
@@ -5,6 +5,7 @@
  */
 
 import React from 'react';
+import React, { memo, useState, useEffect } from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
@@ -22,56 +23,110 @@ import ParticipantCard from './ParticipantCard';
 import Link from './Link';
 import 'style.scss';
 
+const STATUS = ['married', 'single', 'divorced', 'widower'];
+const ORDER = ['increase order', 'decrease order'];
+const ORIGIN = ["a","b","c"];
+
 export function ParticipantDashboard(props) {
   useInjectReducer({ key: 'participantDashboard', reducer });
   useInjectSaga({ key: 'participantDashboard', saga });
+  
+  const [sortedParticipent, setSortedParticipent] = useState([]);
+  participants = React.Children.toArray(props.participants)
 
-  const sorts = ['dob', 'status', 'origin'];
-
-  // const compare = (a, b) => (a.dob >= b.dob ? a : b);
-
-  const applySort = (participants, sort) => {
-    if (!sort) {
-      return participants;
+  useEffect(() => {
+    if (!sortedParticipent.length && participants) {
+      setSortedParticipent(renderList());
     }
-    return React.Children.toArray(props.participants).sort((a, b) =>
-      a.dob >= b.dob ? a : b,
-    );
-  };
+  });
 
-  const SortButtons = sorts.map(sort => (
-    <button
-      type="button"
-      className="sortBtn"
-      onClick={() => applySort(props.participants, sort)}
-      key={sort}
-    >
-      Sort by {sort}
-    </button>
-  ));
+  function getSortedList(sortType, sortBy) {
+      switch (sortType) {
+        case 'dob':
+          sortByDOB(sortBy);
+        case 'status':
+          sortByStatus(sortBy);
+          break;
+        case 'origin':
+          sortByOrigin(sortBy);
+          break;
+        default:
+          renderOriginList();
+      }
+    }
 
-  const ParticipantsCardsArr =
-    props.participants &&
-    props.participants.map(card => (
-      <div className="participantCard">
-        <ParticipantCard
-          key={card.id}
-          name={card.name}
-          dob={card.dob}
-          gender={card.gender}
-          status={card.status}
-          origin={card.origin}
-          email={card.email}
-          phone={card.phone}
-          resume={card.resume}
-        />
-        <button type="button" onClick={() => props.deleteParticipant(card)}>
-          DELETE
-        </button>
-        <Link to={`/updateParticipant/${card.id}`}>UPDATE</Link>
-        <hr />
-      </div>
-    ));
+    function sortByDOB(order) {
+      const sortedParticipentList = participants.sort(
+        (a, b) => new Date(b.dob) - new Date(a.dob),
+      );
+      if (order === 'increase order') {
+        sortedMatchesList.reverse();
+      }
+      setSortedParticipent(renderList(sortedParticipentList));
+    }
+
+    function sortByStatus(status) {
+      const sortedParticipentList = participants.filter(
+        participant => participant.status === status,
+      );
+      setSortedParticipent(renderList(sortedParticipentList)); 
+       }
+
+      function sortByOrigin(origin) {
+        const sortedParticipentList = participants.filter(
+          participant => participant.origin === origin,
+     );
+     setSortedParticipent(renderList(sortedParticipentList));
+        }
+  
+    function renderOriginList() {
+      setSortedParticipent(renderList(participants));
+        }
+
+   function renderList(sortedParticipentList = participant) {
+    return (
+      sortedParticipentList &&
+      sortedParticipentList.map(card => (
+        <div className="participantCard">
+          <ParticipantCard
+            key={card.id}
+            name={card.name}
+            dob={card.dob}
+            gender={card.gender}
+            status={card.status}
+            origin={card.origin}
+            email={card.email}
+            phone={card.phone}
+            resume={card.resume}
+            />
+            <button type="button" onClick={() => props.deleteParticipant(card)}>
+              DELETE
+            </button>
+            <Link to={`/updateParticipant/${card.id}`}>UPDATE</Link>
+            <hr />
+          </div>
+          ))
+         )
+    }
+
+    function getDropDown(title, sortType, ITEMS) {
+      return (
+        <DropdownButton
+          id="dropdown-basic-button"
+          title={title}
+          onSelect={e => {
+            getSortedList(sortType, sortBy);
+          }}
+        >
+          {ITEMS.map(item => (
+            <Dropdown.Item eventKey={item} key={item}>
+              {item}
+            </Dropdown.Item>
+          ))}
+        </DropdownButton>
+      );
+    }
+
   try {
     return (
       <div>
@@ -79,8 +134,12 @@ export function ParticipantDashboard(props) {
           <h3>
             <FormattedMessage {...messages.header} />
           </h3>
-          <div>{SortButtons}</div>
-          {ParticipantsCardsArr}
+          <div className="sortBtn">
+          {getDropDown('Sort by birthdate', 'dob'   , ORDER)}
+          {getDropDown('Sort/Filter by status' , 'status' , STATUS)}
+          {getDropDown('Sort/Filter by origin' , 'origin' , ORIGIN)}
+          </div>
+          {sortedParticipent}
         </center>
       </div>
     );
